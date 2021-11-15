74f5e9c05a39bfa2a16219d89e2c51dbbbb40b21
Vtest <Itest>
2021-11-15 20:20:02 +0300

    Some changes

---
+++ Wiipuri/MSL/MSL/MSLApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSLApp.swift
+//  MSL
+//
+//  Created by Владимир Ильин on 15.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct MSLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
