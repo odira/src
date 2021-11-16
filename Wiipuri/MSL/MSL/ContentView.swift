d53d4887149cd57fae85b5cc8d23de6b0d89abbc
odira <odira@mail.ru>
2021-11-16 13:27:11 +0300

    Some changes

--- Wiipuri/MSL/MSL/ContentView.swift
+++ Wiipuri/MSL/MSL/ContentView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 import Combine
 
 struct ContentView: View {
-    @State private var minPressureACC: String = ""
+    @State private var pressureACC: String = ""
     @State private var minSafeLevelACC: String = "---"
     @State private var buttonIsValid = false
     
@@ -15,31 +15,35 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("ACC Minimum Safe Level")
-                .font(.largeTitle)
-            HStack {
-                Text("FL")
-                    .font(.largeTitle)
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                Text("\(minSafeLevelACC)")
-                    .font(.largeTitle)
-                    .bold()
-                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    .tracking(10)
-            }
-            .padding()
-            TextField("Enter minimum pressure here", text: $minPressureACC)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .border(.black)
+                .font(.title)
+            Text("FL \(minSafeLevelACC)")
+                .bold()
+                .frame(maxWidth: .infinity)
+                .font(.title)
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
                 .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.blue, lineWidth: 3)
+                )
+            Text("Minimum Pressure")
+                .font(.title)
+            TextField("Enter minimum pressure here", text: $pressureACC)
+                .font(.title)
+                .padding()
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.blue, lineWidth: 3)
+                )
                 .keyboardType(.numberPad)
-                .onReceive(Just(minPressureACC)) { newPressure in
-                    let filtered = newPressure.filter { "0123456789".contains($0) }
-                    if filtered != newPressure {
-                        self.minPressureACC = filtered
+                .onReceive(Just(pressureACC)) { pressure in
+                    let filtered = pressure.filter { "0123456789".contains($0) }
+                    if filtered != pressure {
+                        self.pressureACC = filtered
                     }
                 }
             Button(action: {
-                let pressure = Double(self.minPressureACC)!
+                let pressure = Double(self.pressureACC)!
                 if pressure < 977.3 {
                     minSafeLevelACC = "080"
                 } else if pressure < 999.9 {
@@ -47,13 +51,18 @@ struct ContentView: View {
                 } else {
                     minSafeLevelACC = "060"
                 }
-            }) {
-                Text("Calculate MSL")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(.blue)
-                    .cornerRadius(15)
-            }.disabled(!buttonIsValid)
+            }, label: {
+                HStack {
+                    Image(systemName: "staroflife.circle.fill")
+                    Text("Calculate MSL")
+                        .font(.title)
+                }
+            })
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(10)
         }
         .padding()
     }
@@ -64,6 +73,8 @@ struct ContentView_Previews: PreviewProvider {
         Group {
             ContentView()
                 .preferredColorScheme(.light)
+            ContentView()
+                .preferredColorScheme(.dark)
         }
     }
 }
