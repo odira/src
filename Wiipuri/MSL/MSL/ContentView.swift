b0a8f569a76825151b984295adef0829e5ae5126
Vtest <Itest>
2021-11-17 16:45:28 +0300

    Some changes

--- Wiipuri/MSL/MSL/ContentView.swift
+++ Wiipuri/MSL/MSL/ContentView.swift
@@ -3,8 +3,13 @@ import Combine
 
 struct ContentView: View {
     @State private var pressureACC: String = ""
-    @State private var minSafeLevelACC: String = "---"
+    @State private var minSafeLevelACC: String = ""
     @State private var buttonIsValid = false
+    
+    enum Focus: Hashable {
+        case focused
+    }
+    @FocusState private var isFocused: Focus?
     
 //    let pressureFormatter: NumberFormatter = {
 //        let pressureFormatter = NumberFormatter()
@@ -13,58 +18,87 @@ struct ContentView: View {
 //    }()
     
     var body: some View {
-        VStack {
-            Text("ACC Minimum Safe Level")
-                .font(.title)
-            Text("FL \(minSafeLevelACC)")
-                .bold()
-                .frame(maxWidth: .infinity)
-                .font(.title)
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                .padding()
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.blue, lineWidth: 3)
-                )
-            Text("Minimum Pressure")
-                .font(.title)
-            TextField("Enter minimum pressure here", text: $pressureACC)
-                .font(.title)
-                .padding()
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(Color.blue, lineWidth: 3)
-                )
-                .keyboardType(.numberPad)
-                .onReceive(Just(pressureACC)) { pressure in
-                    let filtered = pressure.filter { "0123456789".contains($0) }
-                    if filtered != pressure {
-                        self.pressureACC = filtered
+        TabView {
+            VStack(spacing: 20) {
+                Text("ACC Minimum Safe Level")
+                    .font(.title)
+                Text("FL \(minSafeLevelACC)")
+                    .bold()
+                    .frame(maxWidth: .infinity)
+                    .font(.title)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.blue, lineWidth: 3)
+                    )
+                Text("Minimum Pressure")
+                    .font(.title)
+                TextField("Enter minimum pressure here", text: $pressureACC)
+                    .font(.title)
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.blue, lineWidth: 3)
+                    )
+                    .keyboardType(.numberPad)
+                    .onReceive(Just(pressureACC)) { pressure in
+                        let filtered = pressure.filter { "0123456789".contains($0) }
+                        print(filtered)
+                        if filtered != pressure {
+                            self.pressureACC = filtered
+                        }
                     }
-                }
-            Button(action: {
-                let pressure = Double(self.pressureACC)!
-                if pressure < 977.3 {
-                    minSafeLevelACC = "080"
-                } else if pressure < 999.9 {
-                    minSafeLevelACC = "070"
-                } else {
-                    minSafeLevelACC = "060"
-                }
-            }, label: {
-                HStack {
-                    Image(systemName: "staroflife.circle.fill")
-                    Text("Calculate MSL")
-                        .font(.title)
-                }
-            })
-                .padding()
-                .frame(maxWidth: .infinity)
-                .background(Color.green)
-                .foregroundColor(.white)
-                .cornerRadius(10)
+                    .onAppear {
+                        isFocused = .focused
+                    }
+                    .focused($isFocused, equals: .focused)
+                Button(action: {
+                    let pressure = Double(self.pressureACC)!
+                    if pressure < 977.3 {
+                        minSafeLevelACC = "080"
+                    } else if pressure < 999.9 {
+                        minSafeLevelACC = "070"
+                    } else {
+                        minSafeLevelACC = "060"
+                    }
+                }, label: {
+                    HStack {
+                        Image(systemName: "staroflife.circle.fill")
+                        Text("Calculate MSL")
+                            .font(.title)
+                    }
+                })
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            .padding()
+            .tabItem {
+                Image(systemName: "keyboard.fill")
+                Text("MSL")
+            }
+            
+            VStack {
+                
+            }
+            .tabItem {
+                Image(systemName: "book.closed.fill")
+                Text("AIP")
+            }
+            
+            VStack {
+                Image("20201203 МДП")
+                    .resizable()
+                    .scaledToFit()
+            }
+            .tabItem {
+                Image(systemName: "map.fill")
+                Text("Map")
+            }
         }
-        .padding()
     }
 }
 
